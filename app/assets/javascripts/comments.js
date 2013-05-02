c7ed4fb5a6e26e88ba90cabb497f6ef3924cef3b
Mathilda Thompson <mathildathompson288@Mathildas-MacBook-Pro.local>
2013-05-02 18:55:55 +0100

    Added stuff

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 $('body').bind('ajax:success', function(e, cool_beans) {
 	//remember cool_beans is a div, so you are putting a div inside a temporary div and accessing children() 
 	console.log(cool_beans);
-	var new_comment = $('<div>').append(cool_beans).children().first().data("data-item-id");
+	var new_comment = $('<div>').append(cool_beans).children().data("item-id");
 	$('.comments-' + new_comment).append(cool_beans);
 	
   });
---
+++ app/assets/javascripts/search.js
@@ -0,0 +1,46 @@
+$(document).ready(function(){
+
+	$('#search').change(function(){
+		var searchTerm = $(this).val();
+		console.log(searchTerm);
+	
+	});
+
+});
+
+
+
+
+
+//the change() means that the function is only run when the value is changed
+
+//_.where(items, {name : "Drink a pickleback"})
+//-That works with exact matches
+//filter
+//_.filter(items, function(item){
+	//return item.name.length > 3});
+//_.filter(items, function(item){
+	//return item.name.match(/drink/)});
+// the item in filter about, think of an an individual item
+
+//var results = _.filter(items, function(item){
+	//return item.name.match(RegExp($(this).val());
+		//this represents the input comes from #search
+//})
+
+//search Javascript regular expression match
+
+//$('.items').html('')
+//clears the items on the page
+
+//iterate over results and create html to put them back in
+
+//_.each(results, function(item){
+	//var html= "<p>" + item.name = "<p>";
+	//$('.items').append(html);
+//})
+
+//You have to cope the copy comments so that the styling is the same
+//Change the name of the comment using _.template(long string and replace name with <%= name %>)
+//var output = html({name :item.name});
+//$('items').append(output);
