c17be03ac52de3a564d8729a89e302dc955c84c5
Mathilda Thompson <mathildathompson288@gmail.com>
2013-07-17 11:09:28 +0100

    AJAX

--- app/assets/javascripts/destinations.js
+++ app/assets/javascripts/destinations.js
@@ -1,16 +1,17 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
-// $(document).ready(function(){
-//   console.log('hello');
+$(document).ready(function(){
+  console.log('hello');
  
-//   // $('div').append
-//   $('.destinations').click(function(){
-//   	// console.log(<%=destination.id%>);
-//   	console.log(this.id);
-//   	// console.log(event);
-//   	console.log('blaahhhh');
+  // $('div').append
+  $('.destinations').click(function(){
+  	// console.log(<%=destination.id%>);
+  	console.log(this);
+  	console.log(this.id);
 
-//   	var destination_id = $('.destinations').data('destination-id');
-//   	console.log(destination_id);
-//   })
-// });
+  	// console.log(event);
+
+  	var destination_id = $('.destinations').data('destination-id');
+  	console.log(destination_id);
+  })
+});
